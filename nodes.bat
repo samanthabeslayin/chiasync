e8fe5530d23cdbcae18545c40ae9219ddcaa373a
samanthabeslayin <84518710+samanthabeslayin@users.noreply.github.com>
2021-05-20 09:53:23 -0500

    Add files via upload

---
+++ nodes.bat
@@ -0,0 +1,53 @@
+u/echo off
+
+cls
+
+:start
+
+​chia show -a introducer-or.chia.net:8444
+chia show -a node.chia.net:8444
+chia show -a node-eu.chia.net:8444
+chia show -a  71.194.193.24:8444
+chia show -a  172.222.12.214:8444
+chia show -a  47.12.141.196:8444
+chia show -a  69.253.175.214:8444
+chia show -a  69.125.155.54:8444
+chia show -a  66.176.212.114:8444
+chia show -a  24.159.193.218:8444
+chia show -a  24.14.20.199:8444
+chia show -a  24.183.162.100:8444
+chia show -a  74.74.102.53:8444
+chia show -a  142.54.189.82:8444
+chia show -a  76.114.127.230:8444
+chia show -a  3.238.177.73:8444
+chia show -a  100.36.231.21:8444
+chia show -a  108.196.200.246:8444
+chia show -a  73.211.90.106:8444
+chia show -a  205.144.210.67:8444
+chia show -a  73.137.146.181:8444
+chia show -a  24.117.122.132:8444
+chia show -a  70.227.90.226:8444
+chia show -a  136.55.158.0:8444
+chia show -a  98.193.218.31:8444
+chia show -a  96.241.43.221:8444
+chia show -a  165.227.31.51:8444
+chia show -a  98.189.245.196:8444
+chia show -a  108.44.249.135:8444
+chia show -a  173.225.108.230:8444
+chia show -a  68.117.214.61:8444
+chia show -a  76.244.43.238:8444
+chia show -a  73.181.86.196:8444
+chia show -a  45.36.132.124:8444
+chia show -a  184.99.77.155:8444
+chia show -a  96.253.121.62:8444
+chia show -a  38.34.113.148:8444
+chia show -a  69.117.194.184:8444
+chia show -a  69.112.124.131:8444
+chia show -a  198.199.80.49:8444
+chia show -a  70.188.164.68:8444
+chia show -a  67.240.3.143:8444
+chia show -a  70.140.25.254:8444
+
+u/ping -n 1800 localhost> nul
+
+goto start
---
+++ start_nodes.bat
@@ -0,0 +1,5 @@
+u/echo off
+
+cd C:\Users\Samantha\AppData\Local\chia-blockchain\app-1.1.5\resources\app.asar.unpacked\daemon
+
+C:\Users\Samantha\AppData\Local\chia-blockchain\app-1.1.5\resources\app.asar.unpacked\daemon\nodes.bat
